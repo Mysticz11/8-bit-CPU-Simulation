45aa1f45b83caaa4c2ff2bf37578cb97dc49ee15
Omar <omarbaba2006@gmail.com>
2026-08-27 17:04:03 -0700

    CLI

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
+import argparse
 from CPU import CPU
 from assembler import Assembler
+
+# Lets us chagne the program run from the terminal insteaf of changing the file path each time
+# RUN WITH python main.py theStuff.txt --debug
+
+
+# This is the parser object
+parser = argparse.ArgumentParser(description="8-bit CPU Simulator")
+# Tells the parse that the user needs to give a filename
+parser.add_argument("file", help="Path to .asm file")
+# - - means the debug optional, if not provided we assume true
+parser.add_argument("--debug", action="store_true", help="Run in debug mode")
+# this reads the stuff written in the terminal
+args = parser.parse_args()
 
 assemble = Assembler()
 cpu = CPU()
 
-# Countdown from 5 to 0
-source = """
-LOAD R0, 5
-LOAD R1, 1
-loop:
-SUB R0, R1
-JZ end
-JMP loop
-end:
-HLT
-"""
+with open(args.file, "r") as file:
+        source = file.read()
+
 program = assemble.assemble(source)
 
 cpu.load_program(program)
 
-debug_bool = input("Would you like to use the debugger?\nType Y for yes and N for no: ")
+# debug_bool = input("Would you like to use the debugger?\nType Y for yes and N for no: ")
 
-if debug_bool == 'Y':
-    debug_bool = True
-else:
-    debug_bool = False
+# if debug_bool == 'Y':
+#     debug_bool = True
+# else:
+#     debug_bool = False
 
-cpu.run(debug=debug_bool)
+cpu.run(debug = args.debug)
 
 print(f"R0: {cpu.reg.read('R0')}")  # Should be 0
 print(f"R1: {cpu.reg.read('R1')}")  # Should be 1
